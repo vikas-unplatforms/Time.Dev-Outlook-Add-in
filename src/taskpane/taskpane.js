3948ea63a36599fbb33389ac0f1e91f3b1f526c4
Vikas Kamarapu <vikas.kamarapu@unplatforms.com>
2023-01-13 15:27:00 +0530

    manifest added

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Outlook) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+export async function run() {
+  /**
+   * Insert your Outlook code here
+   */
+  Office.context.mailbox.item.body.setAsync(
+    "Hii from Time.Dev",
+    {
+      coercionType: "html", // Write text as HTML
+    },
+
+    // Callback method to check that setAsync succeeded
+    function (asyncResult) {
+      if (asyncResult.status ==
+        Office.AsyncResultStatus.Failed) {
+        write(asyncResult.error.message);
+      }
+    }
+  );
+}
