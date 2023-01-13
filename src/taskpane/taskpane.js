de71b0dd27f656d2c38b72818767ee8c337b917f
Vikas Kamarapu <vikas.kamarapu@unplatforms.com>
2023-01-13 15:53:11 +0530

    changes in taskpane

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -6,11 +6,11 @@
 /* global document, Office */
 
 Office.onReady((info) => {
-  if (info.host === Office.HostType.Outlook) {
+ 
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
-  }
+
 });
 
 export async function run() {
